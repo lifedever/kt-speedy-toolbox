2dfa5405bd695af5103b54455a74479215d14963
Timothy.Ge <gefangshuai@outlook.com>
2024-08-27 13:49:41 +0800

    🚀deploy

--- kt-speedy-util/src/main/kotlin/kt/speedy/toolbox/util/FileExt.kt
+++ kt-speedy-util/src/main/kotlin/kt/speedy/toolbox/util/FileExt.kt
@@ -1,6 +1,8 @@
 package kt.speedy.toolbox.util
 
+import java.io.ByteArrayOutputStream
 import java.io.File
+import javax.imageio.ImageIO
 
 /**
  * 临时生成文件
@@ -17,4 +19,38 @@ fun File.isImageFile(): Boolean {
     val imageExtensions = listOf("jpg", "jpeg", "png", "gif", "bmp", "webp")
     val extension = this.extension.lowercase()
     return imageExtensions.contains(extension)
+}
+
+fun String.filterFiles(): Boolean {
+    val filter = arrayOf(
+        "__MACOSX",
+    )
+
+    return filter.none { it == this } && !this.startsWith(".")
+}
+
+fun File.getFileType(): String {
+    return this.name.substringAfterLast('.')
+}
+
+fun String.toFile(): File {
+    return File(this)
+}
+
+fun File.imageCut(xRate: Double, yRate: Double, wRate: Double, hRate: Double): ByteArray {
+    val out = ByteArrayOutputStream()
+    val bufferedImage = ImageIO.read(this)
+
+    val imageWidth = bufferedImage.width
+    val imageHeight = bufferedImage.height
+
+    val x = (imageWidth * xRate).toInt()
+    val y = (imageHeight * yRate).toInt()
+
+    val w = (imageWidth * wRate).toInt()
+    val h = (imageHeight * hRate).toInt()
+
+    val cutImage = bufferedImage.getSubimage(x, y, w, h)
+    ImageIO.write(cutImage, this.getFileType(), out)
+    return out.toByteArray()
 }
