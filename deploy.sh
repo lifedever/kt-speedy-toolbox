99f22512c7c3893465987fbcd9f134a221e2d612
Timothy.Ge <gefangshuai@outlook.com>
2024-08-29 11:22:18 +0800

    🚀deploy

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 git add . && git commit -m "🚀deploy"
 git checkout master && git merge --no-edit develop && git checkout develop
 git push origin --all
-mvn clean install org.apache.maven.plugins:maven-source-plugin:jar org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests
+mvn clean install org.apache.maven.plugins:maven-source-plugin:jar org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests -Pdeploy-to-manyibar
+mvn clean install org.apache.maven.plugins:maven-source-plugin:jar org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests -Pdeploy-to-kanasinfo
